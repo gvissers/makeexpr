8ef087b77a91218a156ad6cf542d53d196650885
Gé Vissers <ge@q1t.nl>
2020-02-26 13:07:08 +0100

    Add a bit of documentation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+//! A program for creating arithemtic expression that evaluate to a certain number,
+//!
+//! This program receives a list of input numbers and a target number. Using
+//! all input numbers it creates an arithmetic expression using only addition,
+//! subtraction, multiplication and division operations, that evaluates to the
+//! target number. In case this is not possible, it finds the expression that
+//! evaluates to a number that is a close to the target as possible.
+//!
+//! The program can be used by passing the input numbers and the target number
+//! on the command line:
+//! ```
+//! makeexpr number [number ...] target
+//! ```
+//! The best expression found will then be printed on stdout.
+//! As an example,
+//! ```
+//! makeexpr 1 3 4 6 24
+//! ```
+//! will result in output
+//! ```
+//! 6/(1-3/4) = 24
+//! ```
+
+
 use std::collections::HashMap;
 use arrayvec::ArrayVec;
 use fasthash::xx::Hash64;
 use num_traits::Zero;
 
+/// Type alias for a rational number (i.e. fraction)
 type Rat = num_rational::Ratio<u64>;
 /// Type for an index.
 ///
